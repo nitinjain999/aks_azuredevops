95f595184c507683eeeaaa613c98b99ba4530667
Nitin Jain <nitin_jain999@yahoo.co.in>
2021-12-28 07:37:03 +0100

    Update 02-variables.tf

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -32,3 +32,16 @@ variable "ssh_public_key" {
   #default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
   description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
 }
+# Windows Admin Username for k8s worker nodes
+variable "windows_admin_username" {
+  type = string
+  default = "azureuser"
+  description = "This variable defines the Windows admin username k8s Worker nodes"  
+}
+
+# Windows Admin Password for k8s worker nodes
+variable "windows_admin_password" {
+  type = string
+  default = "P@ssw0rd1234"
+  description = "This variable defines the Windows admin password k8s Worker nodes"  
+}
