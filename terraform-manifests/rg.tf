bb72701dcac6a97e39637dae6b8ed70a1f300357
Nitin Jain <nitin.jain@windriver.com>
2021-12-28 14:39:49 +0100

    Fixing

--- terraform-manifests/rg.tf
+++ terraform-manifests/rg.tf
@@ -1,4 +1,4 @@
-form Resource to Create Azure Resource Group with Input Variables defined in variables.tf
+# Terraform Resource to Create Azure Resource Group with Input Variables defined in variables.tf
 resource "azurerm_resource_group" "aks_rg" {
   name = "${var.resource_group_name}-${var.environment}"
   location = var.location
